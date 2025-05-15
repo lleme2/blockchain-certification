778198f53c52ddaaa5c6d05b3781c9f9ffa35c69
Lucas Leme <lucaslopesleme4@gmail.com>
2025-05-15 18:12:06 -0300

    Implementa h1 e h2 na home do frontend

---
+++ frontend/src/app/components/Grid1.tsx
@@ -0,0 +1,27 @@
+function Grid1() {
+  return (
+    <div className="h2_color">
+      <div className="container-fluid py-5 px-4 px-md-5">
+        <div className="row align-items-center">
+          <div className="col-md-6">
+            <h2 className="fw-bold mb-3">O que é Blockchain?</h2>
+            <p className="fs-5">
+              Blockchain é um sistema de registro distribuído que garante a
+              integridade e transparência dos dados. Aqui você pode aprender
+              todos os conceitos e entender como essa tecnologia funciona!
+            </p>
+          </div>
+          <div className="col-md-6 text-center">
+            <img
+              src="/img1.png"
+              alt="Ilustração de Blockchain"
+              className="img-fluid"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Grid1;
