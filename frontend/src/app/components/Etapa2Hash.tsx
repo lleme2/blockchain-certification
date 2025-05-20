e5b49695253d12790c0edc6ec503195be37e0275
Lucas Leme <lucaslopesleme4@gmail.com>
2025-05-20 20:00:20 -0300

    Corrije de bugs da endpoint de certificacao

--- frontend/src/app/components/Etapa2Hash.tsx
+++ frontend/src/app/components/Etapa2Hash.tsx
@@ -6,7 +6,44 @@ type ComponenteBProps = {
   dado: string;
 };
 
+type ResponseType = {
+  data: {
+    transactionHash?: string;
+  };
+};
+
 export default function Etapa2Hash({ dado }: ComponenteBProps) {
+  const [hashTransacao, setHashTransacao] = useState(
+    "Aqui você verá a hash da transação"
+  );
+  const [hashDocumento, setHashDocumento] = useState(
+    "Aqui você verá a Hash do seu documento."
+  );
+  const [response_data, setResponse] = useState<ResponseType | null>(null);
+
+  // Atualiza hashDocumento quando dado muda
+  useEffect(() => {
+    if (dado && dado.trim() !== "") {
+      setHashDocumento("Hash do documento: " + dado);
+      setHashTransacao("Aqui você verá a hash da transação");
+    } else {
+      setHashDocumento("Aqui você verá a Hash do seu documento.");
+    }
+  }, [dado]);
+
+  // Atualiza hashTransacao quando response mudar
+  useEffect(() => {
+    if (
+      response_data?.data?.transactionHash &&
+      response_data.data.transactionHash.trim() !== ""
+    ) {
+      setHashTransacao(
+        "Hash da transação: " + response_data.data.transactionHash
+      );
+    } else {
+      setHashTransacao("Aqui você verá a hash da transação");
+    }
+  }, [response_data]);
   const hash = dado;
   const handleClick = async () => {
     try {
@@ -14,6 +51,7 @@ export default function Etapa2Hash({ dado }: ComponenteBProps) {
         hash: hash,
       });
       console.log(response);
+      setResponse(response);
     } catch (error) {
       console.log("Hash salva: " + hash);
       console.error("Erro ao enviar:", error);
@@ -21,12 +59,10 @@ export default function Etapa2Hash({ dado }: ComponenteBProps) {
   };
 
   const teste = async () => {
-    try {
-      const response = await axios.get("http://localhost:8080/arquivos");
-      console.log(response);
-    } catch (err) {
-      console.log(err);
-    }
+    setHashTransacao(
+      "0xb1fa2af5f1d6f1ac1c467c8209412c12c816f3f2a2b658ea2ee3bb9b59259c6e"
+    );
+    console.log(hashTransacao);
   };
 
   return (
@@ -34,9 +70,13 @@ export default function Etapa2Hash({ dado }: ComponenteBProps) {
       <div className="etapa-numero">2</div>
       <h5 className="fw-bold mb-3 ">Hash do arquivo</h5>
       <input
-        type="text"
-        value={dado || "Aqui você verá a Hash do seu documento."}
-        className="form-control mb-2 text-center conteudo-topo"
+        className="form-control pt-3 pb-3 text-center conteudo-topo"
+        value={hashDocumento}
+        readOnly
+      />
+      <input
+        className="form-control pt-3 pb-3 text-center conteudo-topo"
+        value={hashTransacao}
         readOnly
       />
       <a href="#" className="small text-primary mb-2 d-block tag-a">
