fd9f01c1614412dbe7692e31f7e2dffd4e1ee6d4
Lucas Leme <lucaslopesleme4@gmail.com>
2025-05-20 20:31:34 -0300

    Adiciona envento de clique etapa4

--- frontend/src/app/components/Etapa3Verificacao.tsx
+++ frontend/src/app/components/Etapa3Verificacao.tsx
@@ -1,4 +1,31 @@
+"use client";
+import axios, { AxiosResponse } from "axios";
+import { useState, useEffect } from "react";
+
+type VerifyResponse = {
+  documentHash?: string;
+  // outros campos que vierem na resposta
+};
+
 export default function Etapa3Verificacao() {
+  const [valor, setValor] = useState("");
+
+  async function handleEnviar(): Promise<void> {
+    try {
+      const response: AxiosResponse<VerifyResponse> = await axios.get(
+        `/verify/${valor}`
+      );
+      console.log("Resposta:", response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setValor(event.target.value);
+    setValor(event.target.value); // pega o valor digitado
+  }
+
   return (
     <div className="card text-center shadow-sm p-4 card-etapa">
       <div className="etapa-numero">3</div>
@@ -6,9 +33,13 @@ export default function Etapa3Verificacao() {
       <input
         type="text"
         placeholder="Hash ou arquivo do documento"
+        value={valor}
         className="form-control mb-2 conteudo-topo mb-3"
+        onChange={handleChange}
       />
-      <button className="btn btn-primary btn-sm w-100">Verificar</button>
+      <button className="btn btn-primary btn-sm w-100" onClick={handleEnviar}>
+        Verificar
+      </button>
     </div>
   );
 }
